1acb0c75255bdee419fac7cf0696b9a2f7779e78
Inagaki Asami <202504ap01_inagaki@x-ide.sakura.ne.jp>
2025-07-02 14:56:34 +0900

    郵便番号書式チェック修正

--- postalcodesearch.js
+++ postalcodesearch.js
@@ -5,7 +5,12 @@ document.addEventListener('DOMContentLoaded', function () {
         if (!zip) {
             alert('郵便番号を入力してください');
             return;
+        } else if (zip.match(/^\d{3}-\d{4}$/) == null) {
+            alert('郵便番号が正しくありません');
+            return;
         }
+
+
 
         fetch('Searchaddress.php', {
             method: 'POST',
