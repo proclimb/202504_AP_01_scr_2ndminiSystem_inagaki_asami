f57629d0e299f8f5b3db360bbacf3eb6b2e8970f
Inagaki Asami <202504ap01_inagaki@x-ide.sakura.ne.jp>
2025-07-15 11:49:47 +0900

    全体フォームの修正（</div>タブの削除)

--- edit.php
+++ edit.php
@@ -123,7 +123,7 @@ $_POST = $user->findById($id);
                     ?>
 
 
-                    <div class="birth-selects" style="display: flex; gap: 10px;">
+                    <div class="birth-selects">
                         <!-- 年 -->
                         <select name="birth_year">
                             <option value="">-- 年 --</option>
@@ -157,93 +157,92 @@ $_POST = $user->findById($id);
                             ?>
                         </select>
                     </div>
-                </div>
 
-            </div>
-            <div>
-                <label>郵便番号<span>必須</span></label>
-                <div class="postal-row">
+                </div>
+                <div>
+                    <label>郵便番号<span>必須</span></label>
+                    <div class="postal-row">
+                        <input
+                            class="half-width"
+                            type="text"
+                            name="postal_code"
+                            id="postal_code"
+                            placeholder="例）100-0001"
+                            value="<?= htmlspecialchars($_POST['postal_code'] ?? '') ?>">
+                        <button type="button"
+                            class="postal-code-search"
+                            id="searchAddressBtn">住所検索</button>
+                    </div>
+                </div>
+                <div>
+                    <label>住所<span>必須</span></label>
                     <input
-                        class="half-width"
                         type="text"
-                        name="postal_code"
-                        id="postal_code"
-                        placeholder="例）100-0001"
-                        value="<?= htmlspecialchars($_POST['postal_code'] ?? '') ?>">
-                    <button type="button"
-                        class="postal-code-search"
-                        id="searchAddressBtn">住所検索</button>
+                        name="prefecture"
+                        id="prefecture"
+                        placeholder="都道府県"
+                        value="<?= htmlspecialchars($_POST['prefecture'] ?? '') ?>">
+                    <input
+                        type="text"
+                        name="city_town"
+                        id="city_town"
+                        placeholder="市区町村・番地"
+                        value="<?= htmlspecialchars($_POST['city_town'] ?? '') ?>">
+                    <input
+                        type="text"
+                        name="building"
+                        placeholder="建物名・部屋番号  **省略可**"
+                        value="<?= htmlspecialchars($_POST['building'] ?? '') ?>">
                 </div>
-            </div>
-            <div>
-                <label>住所<span>必須</span></label>
-                <input
-                    type="text"
-                    name="prefecture"
-                    id="prefecture"
-                    placeholder="都道府県"
-                    value="<?= htmlspecialchars($_POST['prefecture'] ?? '') ?>">
-                <input
-                    type="text"
-                    name="city_town"
-                    id="city_town"
-                    placeholder="市区町村・番地"
-                    value="<?= htmlspecialchars($_POST['city_town'] ?? '') ?>">
-                <input
-                    type="text"
-                    name="building"
-                    placeholder="建物名・部屋番号  **省略可**"
-                    value="<?= htmlspecialchars($_POST['building'] ?? '') ?>">
-            </div>
-            <div>
-                <label>電話番号<span>必須</span></label>
-                <input
-                    type="text"
-                    name="tel"
-                    placeholder="例）000-000-0000"
-                    value="<?= htmlspecialchars($_POST['tel']) ?>">
-            </div>
-            <div>
-                <label>メールアドレス<span>必須</span></label>
-                <input
-                    type="text"
-                    name="email"
-                    placeholder="例）guest@example.com"
-                    value="<?= htmlspecialchars($_POST['email']) ?>">
-            </div>
-            <div>
-                <label>本人確認書類（表）</label>
-                <input
-                    type="file"
-                    name="document1"
-                    id="document1"
-                    accept="image/png, image/jpeg, image/jpg">
-                <span id="filename1" class="filename-display"></span>
-                <div class="preview-container">
-                    <img id="preview1" src="#" alt="プレビュー画像１" style="display: none; max-width: 200px; margin-top: 8px;">
+                <div>
+                    <label>電話番号<span>必須</span></label>
+                    <input
+                        type="text"
+                        name="tel"
+                        placeholder="例）000-000-0000"
+                        value="<?= htmlspecialchars($_POST['tel']) ?>">
                 </div>
-            </div>
+                <div>
+                    <label>メールアドレス<span>必須</span></label>
+                    <input
+                        type="text"
+                        name="email"
+                        placeholder="例）guest@example.com"
+                        value="<?= htmlspecialchars($_POST['email']) ?>">
+                </div>
+                <div>
+                    <label>本人確認書類（表）</label>
+                    <input
+                        type="file"
+                        name="document1"
+                        id="document1"
+                        accept="image/png, image/jpeg, image/jpg">
+                    <span id="filename1" class="filename-display"></span>
+                    <div class="preview-container">
+                        <img id="preview1" src="#" alt="プレビュー画像１" style="display: none; max-width: 200px; margin-top: 8px;">
+                    </div>
+                </div>
 
-            <div>
-                <label>本人確認書類（裏）</label>
-                <input
-                    type="file"
-                    name="document2"
-                    id="document2"
-                    accept="image/png, image/jpeg, image/jpg">
-                <span id="filename2" class="filename-display"></span>
-                <div class="preview-container">
-                    <img id="preview2" src="#" alt="プレビュー画像２" style="display: none; max-width: 200px; margin-top: 8px;">
+                <div>
+                    <label>本人確認書類（裏）</label>
+                    <input
+                        type="file"
+                        name="document2"
+                        id="document2"
+                        accept="image/png, image/jpeg, image/jpg">
+                    <span id="filename2" class="filename-display"></span>
+                    <div class="preview-container">
+                        <img id="preview2" src="#" alt="プレビュー画像２" style="display: none; max-width: 200px; margin-top: 8px;">
+                    </div>
                 </div>
             </div>
-    </div>
-    <button type="submit">更新</button>
-    <input type="button" value="ダッシュボードに戻る" onclick="history.back(-1)">
-    </form>
-    <form action="delete.php" method="post" name="delete">
-        <input type="hidden" name="id" value="<?php echo $_POST['id'] ?>">
-        <button type="submit">削除</button>
-    </form>
+            <button type="submit">更新</button>
+            <input type="button" value="ダッシュボードに戻る" onclick="history.back(-1)">
+        </form>
+        <form action="delete.php" method="post" name="delete">
+            <input type="hidden" name="id" value="<?php echo $_POST['id'] ?>">
+            <button type="submit">削除</button>
+        </form>
     </div>
 </body>
 
